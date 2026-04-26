357933c4962e1a504ab0cde5deed251c2f85df7a
letienhoang <ltienhoang2@gmail.com>
2026-04-26 22:18:28 +0700

    feat: create a basic layout and navigation

---
+++ OAI/OAI.Web/Components/Pages/Invoices/InvoiceDetail.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace OAI.Web.Components.Pages.Invoices;
+
+public partial class InvoiceDetail : ComponentBase
+{
+    [Parameter]
+    public Guid InvoiceId { get; set; }
+}
