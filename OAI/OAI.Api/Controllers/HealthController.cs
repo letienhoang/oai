83516184c35fc9233ea7725d47a68793be86edcc
letienhoang <ltienhoang2@gmail.com>
2026-05-12 19:01:20 +0700

    T88 Create OAI.Api project

---
+++ OAI/OAI.Api/Controllers/HealthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OAI.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public sealed class HealthController : Controller
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new
+        {
+            status = "Healthy",
+            service = "OAI.Api"
+        });
+    }
+}
